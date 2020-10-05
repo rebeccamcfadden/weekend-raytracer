a8c7918f2941b61fb077538d83ae92636f180033
Rebecca McFadden <rebecca.mcfadden16@gmail.com>
2020-10-04 22:26:45 -0400

    field of view and camera updates

--- camera.h
+++ camera.h
@@ -5,22 +5,24 @@
 
 class camera {
  public:
-  camera() {
-    // Image dimensions
-    const double aspect_ratio = 16.0 / 9.0;
-    // Camera location
-    double view_height = 2.0;
+  camera(pt3 lookfrom, pt3 lookat, vec3 vup, double fov, double aspect_ratio) {
+    auto theta = deg2rad(fov);
+    auto h = tan(theta / 2);
+    double view_height = 2.0 * h;
     double view_width = aspect_ratio * view_height;
-    double focal_length = 1.0;
 
-    origin = pt3(0, 0, 0);
-    horizontal = vec3(view_width, 0, 0);
-    vertical = vec3(0, view_height, 0);
-    lower_left = origin - (horizontal / 2) - (vertical / 2) - vec3(0, 0, focal_length);
+    auto w = unit(lookfrom - lookat);
+    auto u = unit(vup.cross(w));
+    auto v = w.cross(u);
+
+    origin = lookfrom;
+    horizontal = view_width * u;
+    vertical = view_height * v;
+    lower_left = origin - (horizontal / 2) - (vertical / 2) - w;
   }
 
-  ray get_ray(double u, double v) const {
-      return ray(origin, lower_left + u*horizontal + v*vertical - origin);
+  ray get_ray(double s, double t) const {
+    return ray(origin, lower_left + s * horizontal + t * vertical - origin);
   }
 
  private:
